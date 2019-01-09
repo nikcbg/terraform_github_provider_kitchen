760308d5f9884ddb97f45cf0d505347f1d134892
nikcbg <43785382+nikcbg@users.noreply.github.com>
2019-01-09 10:08:57 +0200

    Update default_test.rb

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -1,6 +1,6 @@
 control 'check_website' do
 
-  describe http('https://github.com/nikcbg/terraform_github_provider_kitchen') do
+  describe http('https://github.com/nikorganization') do
     its('status') { should cmp 200 }
     its('body') { should match 'My awesome codebase' }
   end
